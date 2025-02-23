0c9a13c25556b35586c83757ddd478960bda6f8b
Emma Adey <eadey@usf.edu>
2025-02-23 15:09:00 -0500

    Task 4 - Implemented Order Management

--- cc_10.js
+++ cc_10.js
@@ -61,13 +61,24 @@ console.log("\nTask 3: Creating an Inventory Class");
 class Inventory { // creating class inventory
     constructor() {
         this.products = [];
-        
+        this.orders = []; // modifying task 4 adding order array
     };
     addProduct(product) { // adds a new product to inventory
         return this.products.push(product); // using ppush method
     };
     listProducts () { // logs all of products details 
         return this.products.forEach(product => console.log(product.getDetails()));
+    }
+    placeOrder(orderId, product, quantity) { // modifying task 4 adding method placeOrder
+        if (product.stock >= quantity) {
+            let order = new Order(orderId, product, quantity); // creating new order
+            this.orders.push(order);
+        } else {
+            return `Stock Unavailable: Stock of ${product.name} is currently ${product.stock}`; // Message returned if unavilable 
+        };
+    };
+    listOrders() { // task 4 adding method listOrders()
+        this.orders.forEach(order => console.log(order.getOrderDetails())); // logging all placed orders
     }
 
 }
@@ -78,3 +89,12 @@ inventory.addProduct(prod1);
 inventory.listProducts();
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
 
+// Task 4: Implementing Order Management
+console.log("\nTask 4: Implementing Order Management");
+
+// Logging test cases outputs:
+inventory.placeOrder(601, prod1, 2);
+inventory.listOrders();
+// Expected output: "Order ID: 601, Product: Laptop, Quantity: 2, Total Price: $2400"
+console.log(prod1.getDetails());
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 3"
