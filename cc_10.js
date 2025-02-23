1ddb46c8f9192394f9af42c4ba97aba553619903
Emma Adey <eadey@usf.edu>
2025-02-23 14:43:31 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -29,4 +29,28 @@ prod1.updateStock(3);
 console.log(prod1.getDetails()); 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
 
+// Task 2: Creating an Order Class
+console.log("\nTask 2: Creating an Order Class");
 
+class Order { // creating a class order
+    constructor(orderId, product, quantity) { // properties in class
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+
+        this.product.updateStock(this.quantity);
+    }
+
+    getOrderDetails() { // method to retun order details
+        return `OrderId: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`;
+    
+    }
+}
+
+// Logging test cases outputs:
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
